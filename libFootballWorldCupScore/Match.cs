3a7f37701cc2248ba86e7ab6fcdd7a9ea96d5c4d
Juan Jesus Salas <jjsalas@isotrol.com>
2022-11-27 18:45:04 +0100

    Create empty library

---
+++ libFootballWorldCupScore/Match.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace libFootballWorldCupScore
+{
+    public class Match
+    {
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public ushort HomeScore { get; set; }
+        public ushort AwayScore { get; set; }
+    }
+}
